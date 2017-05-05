291e78cf6a30e730ac6a86e6db5042c994fffdcb
Oleksiy Pylypenko <oleksiy.pylypenko@gmail.com>
2017-05-05 17:34:50 +0200

    Multiplying uploaders

---
+++ src/main/java/website/ManyWebsiteUploader.java
@@ -0,0 +1,24 @@
+package website;
+
+import java.io.File;
+import java.util.List;
+
+import static java.nio.file.Files.createTempDirectory;
+
+public class ManyWebsiteUploader implements WebsiteUploader {
+    private final List<WebsiteUploader> uploaders;
+
+    public ManyWebsiteUploader(List<WebsiteUploader> uploaders) {
+        this.uploaders = uploaders;
+    }
+
+    @Override
+    public void setPathToDaily(File dailyFiles) {
+        uploaders.forEach(uploader -> uploader.setPathToDaily(dailyFiles));
+    }
+
+    @Override
+    public void update() {
+        uploaders.forEach(WebsiteUploader::update);
+    }
+}
