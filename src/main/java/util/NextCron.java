be2e489743fe45813fc94e5afe1e7bfc55f1c638
Oleksiy Pylypenko <oleksiy.pylypenko@gmail.com>
2017-05-14 15:42:27 +0200

    Next cron script

--- src/main/java/util/NextCron.java
+++ src/main/java/util/NextCron.java
@@ -2,11 +2,15 @@ package util;
 
 import org.springframework.scheduling.support.CronSequenceGenerator;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.stream.Collectors;
 
 public class NextCron {
     public static void main(String[] args) {
-        CronSequenceGenerator seq = new CronSequenceGenerator(args[0]);
+        CronSequenceGenerator seq = new CronSequenceGenerator(
+                Arrays.stream(args).collect(Collectors.joining(" "))
+                );
         Date now = new Date();
         Date next = seq.next(now);
         long delta = next.getTime() - now.getTime();
